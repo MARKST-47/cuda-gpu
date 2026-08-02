9ab052a26500283bbb8ee19d95c490d6add1575b
Mark <markstephenthomas10@gmail.com>
2026-08-02 21:46:37 +0200

    Add compare squares

---
+++ pytorch_square.py
@@ -0,0 +1,58 @@
+import torch
+
+a = torch.tensor([1., 2., 3.])
+print(torch.square(a))  
+print(a ** 2)
+print(a * a)
+
+def time_pytorch_function(func, input):
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+    for _ in range(5):
+        func(input)
+    
+    start.record()
+    func(input)
+    end.record()
+    torch.cuda.synchronize()
+    return start.elapsed_time(end)
+
+b = torch.randn(10000, 10000, device='cuda')
+
+def square_2(a):
+    return a ** 2
+
+def square_3(a):
+    return a * a
+
+time_pytorch_function(torch.square, b)
+time_pytorch_function(square_2, b)
+time_pytorch_function(square_3, b)
+
+print("=============")
+print("Profiling torch.square")
+print("=============")
+
+# Now profile each function using pytorch profiler
+with torch.autograd.profiler.profile(use_cuda=True) as prof:
+    torch.square(b)
+
+print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
+
+print("=============")
+print("Profiling a ** 2")
+print("=============")
+
+with torch.autograd.profiler.profile(use_cuda=True) as prof:
+    square_2(b)
+
+print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
+
+print("=============")
+print("Profiling a * a")
+print("=============")
+
+with torch.autograd.profiler.profile(use_cuda=True) as prof:
+    square_3(b)
+
+print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
